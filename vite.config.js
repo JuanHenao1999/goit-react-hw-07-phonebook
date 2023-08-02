b2e5d9875240096dc78c3ab7731f0e8cf260f905
JuanHenao1999 <juanhenao123kl@gmail.com>
2023-08-02 18:04:41 -0500

    first commit

---
+++ vite.config.js
@@ -0,0 +1,8 @@
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react()],
+  base: 'https://JuanHenao1999.github.io/goit-react-hw-07-phonebook'
+})
